eae58f8b61a6dea8f6619eb9215a04bf2a0d5893
Kishore-Cumulus <kishore@nvidia.com>
2022-04-26 16:16:36 +0530

    removing dist files

--- dist/src/auth/client/top-level-interaction.js
+++ dist/src/auth/client/top-level-interaction.js
@@ -5,14 +5,12 @@ var topLevelInteraction = function (shop, host, prefix) {
   if (prefix === void 0) {
     prefix = '';
   }
-  return (
-    '(function() {\n      function setUpTopLevelInteraction() {\n        var TopLevelInteraction = new ITPHelper({\n          redirectUrl: "' +
-    prefix +
-    '/auth?shop=' +
-    encodeURIComponent(shop) +
-    '&host=' +
-    host +
-    '",\n        });\n\n        TopLevelInteraction.execute();\n      }\n\n      document.addEventListener("DOMContentLoaded", setUpTopLevelInteraction);\n    })();'
-  );
+  return '(function() {\n      function setUpTopLevelInteraction() {\n        var TopLevelInteraction = new ITPHelper({\n          redirectUrl: "'
+    .concat(prefix, '/auth?shop=')
+    .concat(encodeURIComponent(shop), '&host=')
+    .concat(
+      host,
+      '",\n        });\n\n        TopLevelInteraction.execute();\n      }\n\n      document.addEventListener("DOMContentLoaded", setUpTopLevelInteraction);\n    })();',
+    );
 };
 exports.default = topLevelInteraction;
